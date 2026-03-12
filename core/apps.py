7a35648b05d06ff8da8ebabe92aaf5f3723ecb42
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-12 00:40:25 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
+
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        from django.db.models.signals import post_migrate
+        from .signals import seed_cards
+
+        post_migrate.connect(seed_cards, sender=self)

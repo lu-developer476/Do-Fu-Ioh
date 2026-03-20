6a4c1a40e8efbab9b6d6d0aa364a83b8a6ec0e3b
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-20 04:24:53 -0300

    Unify Django staticfiles convention

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ set -o errexit
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+
+# Keep a single static artifact directory to avoid ambiguity with the legacy `staticfiles/` name.
+rm -rf staticfiles .staticfiles
 python manage.py collectstatic --noinput
